33845c0ee801841316e1fa5a5a7c5d3de8402621
yuktnk <yuki0wl@gmail.com>
2020-09-18 13:06:59 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
+import { NONAME } from 'dns';
+import { NO_DATA_ALLOWED_ERR } from 'domexception';
+import { NormalizedMessage } from 'fork-ts-checker-webpack-plugin/lib/NormalizedMessage';
 import React from 'react';
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input 
-//         type="text" 
-//         onChange={() => {console.log("I am clicked");}}
-//         />
-//       </React.Fragment>
-//     )
-//   }
-// }
-
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5},
+    { age: 434}
+  ]
   return (
-    <>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-    </>
+    <React.Fragment>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </React.Fragment>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and {props.age}years old! </div>
 }
 
+User.defaultProps = {
+  name: "NoName",
+  age: "NoAge"
+}
 
 export default App;

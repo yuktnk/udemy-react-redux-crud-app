35b4cbea54805c6efc5035bdb10ccea54e36700f
yuktnk <yuki0wl@gmail.com>
2020-09-18 11:45:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return <h1>Hello, world!</h1>;
   }
 }
+
+
 
 export default App;

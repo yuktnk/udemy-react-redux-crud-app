ff4b527e9786e5d89dece69e25e23132f6db2fa1
yuktnk <yuki0wl@gmail.com>
2020-09-21 08:00:19 +0900

    [apply]material-ui to events-index

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -4,10 +4,21 @@ import React, { Component }from 'react';
 // ViewのEventで状態を遷移させて、遷移後の状態を画面に再描画する。
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
+import FlootingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 // ActionCreatorをimportする
 import { readEvents } from '../actions'
+import { fixed } from 'global-prefix';
 
 class EventsIndex extends Component {
   componentDidMount() {
@@ -16,35 +27,48 @@ class EventsIndex extends Component {
 
   renderEvents() {
     return _.map(this.props.events, event =>(
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableRowColumn>{event.id}</TableRowColumn>
+        <TableRowColumn>
           <Link to={`/events/${event.id}`}>
             {event.title}
           </Link>
-        </td>
-        <td>{event.body}</td>
-      </tr>
+        </TableRowColumn>
+        <TableRowColumn>{event.body}</TableRowColumn>
+      </TableRow>
     ))
   }
 
   render() {
+    const style ={
+      position: "fixed",
+      right: 16,
+      bottom: 16
+    }
     return(
       <React.Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Iitle</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderEvents()}
-          </tbody>
-        </table>
+        <FlootingActionButton style={style} containerElement={<Link to="/events/new" />}>
+          <ContentAdd />
+        </FlootingActionButton>
+        
+        <Table>
+          <TableHeader
+          displaySelectAll={false}
+          adjustForCheckbox={false}
+          >
+            <TableRow>
+              <TableHeaderColumn>ID</TableHeaderColumn>
+              <TableHeaderColumn>Iitle</TableHeaderColumn>
+              <TableHeaderColumn>Body</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
 
-        <Link to="/events/new">New Event</Link>
+          <TableBody displayRowCheckbox={false}>
+            {this.renderEvents()}
+          </TableBody>
+        </Table>
+
+        
       </React.Fragment>
     )
   }

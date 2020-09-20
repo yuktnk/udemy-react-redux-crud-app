44b656f469cc1bef48743f09de936d6490914772
yuktnk <yuki0wl@gmail.com>
2020-09-20 23:33:49 +0900

    implement update feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const READ_EVENT = 'READ_EVENT'
 export const CREATE_EVENT = 'CREATE_EVENT'
 export const DELETE_EVENT = 'DELETE_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
+
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -17,10 +20,25 @@ export const postEvent = values => async dispatch => {
   dispatch({ type: CREATE_EVENT, response })
 }
 
+export const putEvent = values => async dispatch => {
+  const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+  dispatch({ type: UPDATE_EVENT, response })
+}
+
+export const getEvent = id => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({ type: READ_EVENT, response })
+}
+
 export const deleteEvent = id => async dispatch => {
   await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
   dispatch({ type: DELETE_EVENT, id })
 }
+
+
+
+
+
 
 // // Reducerでも使うため、exportしておく
 // // 複数の場所で活用されるデータについては、一箇所で定義しておく
--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,12 @@ class EventsShow extends Component {
     this.onSubmit = this.onSubmit.bind(this)
     this.onDeleteClick = this.onDeleteClick.bind(this)
   }
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) this.props.getEvent(id)
+  }
+
   renderField(field) {
     const { input, label, type, meta: { touched, error } } = field
     return (
@@ -23,7 +29,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values)
+    await this.props.putEvent(values)
     this.props.history.push('/')
   }
   async onDeleteClick() {
@@ -33,14 +39,14 @@ class EventsShow extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
+    const { handleSubmit, pristine, submitting, invalid } = this.props
 
     return(
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
         <div>
-          <input type="submit" value="submit" disabled={pristine || submitting} />
+          <input type="submit" value="submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
         </div>
@@ -56,9 +62,12 @@ const validate = values => {
   return errors
 }
 // src/reducers/index.jsのexport default combineReducers({ count })で指定している。
-const mapDispatchToProps = ({ deleteEvent })
-
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
+const mapStateToProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id]
+  return { initialValues: event, event }
+}
 // ★ StateとActionをコンポーネントに関連付けるための重要な記述 ★
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow)
   );

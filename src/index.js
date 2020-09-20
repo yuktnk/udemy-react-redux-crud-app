79d9ac92f700fc6475927501b9c26f9b767a19a9
yuktnk <yuki0wl@gmail.com>
2020-09-20 20:11:24 +0900

    [add]redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore, applyMiddleware } from 'redux'; // Storeを作成するた
 import { Provider } from 'react-redux'; // 作成したStoreを全コンポーネントに渡すための機能を持つProviderという特殊なコンポーネントをインポート
 import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
 import reducer from './reducers'
@@ -12,9 +12,10 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import * as serviceWorker from './serviceWorker';
 
-//App内の全てのStateを集約する
-const store = createStore(reducer, applyMiddleware(thunk))
 
+//App内の全てのStateを集約する
+const enhancer = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
   // storeがアプリケーション内のどのコンポーネントからも参照できるようにする記述
